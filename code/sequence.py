1ce7959b042998e686ced4cf79b5b2ab7ad02093
Melanie Cebula <melaniecebula@berkeley.edu>
2014-11-09 11:26:03 -0800

    checking whether the smaller string is a suffix of the larger string is faster than checking if the larger string is a prefix

--- code/sequence.py
+++ code/sequence.py
@@ -1,16 +1,23 @@
 import time
+import filecmp
 
 def main():
-    with open("../Dataset/reads4.txt") as f:
-        substrings = [line.rstrip() for line in f]
-        substrings_copy = substrings[:]
-        start = time.time()
-        res = find_string(substrings, substrings_copy)
-        print res
-        end = time.time()
-        print end - start
-        with open("output4.txt", "w") as f:
-            f.write(res +"\n")
+    for i in range(1, 16+1):
+        input_i = "../Dataset/reads" + str(i) + ".txt"
+        with open(input_i) as f:
+            substrings = [line.rstrip() for line in f]
+            substrings_copy = substrings[:]
+            start = time.time()
+            res = find_string(substrings, substrings_copy)
+            end = time.time()
+            print end - start
+            output_i = "../Dataset/output" + str(i) + ".txt"
+            with open(output_i, "w") as f:
+                f.write(res +"\n")
+            if i < 6:
+                print str(i) + str(filecmp.cmp("../Dataset/answer" + str(i) + ".txt", output_i))
+            else:
+                print str(i)
 
 def find_string(substrings, substrings_copy):
     while len(substrings) > 2:
@@ -57,11 +64,10 @@ def get_max_overlap(s1, s2):
         if is_prefix(small[i:len_small+1], large):
             prefix = small[:i]
             pos_res.append((prefix + large, float(2.0*(len_small - len(prefix)))))
-
-    for j in range(len_large):
-        if is_prefix(large[j:len_large+1], small):
-            prefix = large[:j]
-            pos_res.append((prefix + small, float(2.0*(len_large - len(prefix)))))
+    for j in range(len_small, -1, -1):
+        if is_suffix(small[:j], large):
+            suffix = small[j:]
+            pos_res.append((large + suffix, float(2.0*(len_small - len(suffix)))))
     if pos_res:
         res = max(pos_res, key=lambda x: x[1])
         return (res[1], res[0], s1, s2)
@@ -72,7 +78,8 @@ def get_max_overlap(s1, s2):
 def is_prefix(pre, post):
     return post.startswith(pre)
 
+def is_suffix(post, pre):
+    return pre.endswith(post)
+
 if __name__ == "__main__":
     main()
-
-
